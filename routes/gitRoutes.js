2ea67951cf8ab0de2944511474ccd63534d67785
Kapil810 <kapil.pandey@emossy.com>
2024-07-10 21:27:03 +0530

    change route

--- routes/gitRoutes.js
+++ routes/gitRoutes.js
@@ -4,6 +4,6 @@ const{auth,authorize} = require("../config/githubconfig");
 const gitWebNotification = require("../controller/githubwebhook");
 route.get("/github",auth);
 route.get("/git/callback",authorize);
-route.get("/githubwebhook",gitWebNotification);
+route.get("/git/webhook",gitWebNotification);
 
 module.exports = route;
